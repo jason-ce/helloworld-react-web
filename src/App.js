75685fb0be2f186e54176cddfe9c9c6a170099cb
Jason <jason@phantomranch>
2017-02-27 23:35:24 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const shared = require('helloworld-shared');
+
 
 class App extends Component {
   render() {
@@ -8,7 +10,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>first var: {shared.helloText}</h2>
+          <h4>second var: {shared.goodbye.goodbye()}</h4>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +21,5 @@ class App extends Component {
   }
 }
 
+// class App {}
 export default App;
